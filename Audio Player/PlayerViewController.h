d72c3136558f11033f2e4bc50fd9823513edefa7
Denis <denis@MacBook-Pro-Denis.local>
2016-04-07 12:48:18 +0300

    Player vc implemented

--- Audio Player/PlayerViewController.h
+++ Audio Player/PlayerViewController.h
@@ -7,9 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface PlayerViewController : UIViewController
+@interface PlayerViewController : UIViewController <AVAudioPlayerDelegate>{
+    
+    __weak IBOutlet UIView *audioIconView;
+    __weak IBOutlet UIView *controlsView;
+    __weak IBOutlet UIButton *playPauseButton;
+    AVAudioPlayer *player;
+    __weak IBOutlet UISlider *durationSlider;
+    
+    __weak IBOutlet UISlider *volumeSlider;
+    __weak IBOutlet UILabel *currentTimePositionLabel;
+    __weak IBOutlet UILabel *totalDurationLabel;
+    NSTimer *timer;
+}
 
 @property (nonatomic, strong) Audio *selectedAudio;
+
+- (IBAction)playPauseButtonClicked:(id)sender;
+- (IBAction)volumeSliderDidChangeValue:(UISlider *)sender;
+- (IBAction)durationSliderDidChangeValue:(UISlider *)sender;
 
 @end
